161bdfb2c89e2e321e03b47687a4872a3546d5a1
Lassi Timoskainen <timoskainen.lassi@gmail.com>
2020-06-19 22:12:26 +0300

    Added front

---
+++ build/precache-manifest.5a99f947a385ae0425c7ed2ed878f3fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95bba190d1e9aba086fdde9a44a92c89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24dc2b17a0ab39e5f1c1",
+    "url": "/static/css/main.96f5ad85.chunk.css"
+  },
+  {
+    "revision": "852868abd8e11101c0b0",
+    "url": "/static/js/2.1fd0c08b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1fd0c08b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24dc2b17a0ab39e5f1c1",
+    "url": "/static/js/main.d8100014.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
