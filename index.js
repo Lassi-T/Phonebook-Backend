41589bec22db23205b8fff4128dce1e90f461d34
Lassi Timoskainen <timoskainen.lassi@gmail.com>
2020-06-19 11:22:32 +0300

    Added error handeling

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express()
 
 const PORT = 3001
 
+app.use(express.json())
+
 let persons = [
   {
     name: "Arto Hellas",
@@ -22,16 +24,52 @@ let persons = [
   },
 ]
 
+const generateId = () => {
+  Math.floor(Math.random() * Math.floor(1000000))
+}
+
+// Gets the whole phonebook
+app.get('/api/persons', (request, response) => {
+  response.send(persons)
+})
+
+// Gets a specific person from the phonebook
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   const person = persons.find(person => person.id === id)
   person ? response.json(person) : response.status(404).end()
 })
 
-app.get('/api/persons', (request, response) => {
-  response.send(persons)
+// Add a new contact to the phonebook
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'Content missing'
+    })
+  }
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(403).json({
+      error: 'Name must be unigue'
+    })
+  }
+  const newPerson = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+  persons = persons.concat(newPerson)
+  response.json(newPerson)
 })
 
+// Delete a contact from the phonebook
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+// Renders info for the phonebook
 app.get('/info', (request, response) => {
   response.send(
     `<div>
