f78e58d57b696b28254753fb67d01763a7bc12e8
Lassi Timoskainen <timoskainen.lassi@gmail.com>
2020-06-19 21:00:47 +0300

    Implemented morgan middleware

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 const express = require('express')
 const { request, response } = require('express')
+const morgan = require('morgan')
 const app = express()
 
-const PORT = 3001
-
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body'))
+
+morgan.token('body', function (request) {return JSON.stringify(request.body)})
 
 let persons = [
   {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1,
   },
   {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+    id: 2,
   },
   {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+    id: 3,
   },
 ]
 
+// Used to get a random id
 const generateId = () => {
   Math.floor(Math.random() * Math.floor(1000000))
 }
@@ -36,7 +39,7 @@ app.get('/api/persons', (request, response) => {
 // Gets a specific person from the phonebook
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
+  const person = persons.find((person) => person.id === id)
   person ? response.json(person) : response.status(404).end()
 })
 
@@ -45,18 +48,18 @@ app.post('/api/persons', (request, response) => {
   const body = request.body
   if (!body.name || !body.number) {
     return response.status(400).json({
-      error: 'Content missing'
+      error: 'Content missing',
     })
   }
-  if (persons.find(person => person.name === body.name)) {
+  if (persons.find((person) => person.name === body.name)) {
     return response.status(403).json({
-      error: 'Name must be unigue'
+      error: 'Name must be unigue',
     })
   }
   const newPerson = {
     name: body.name,
     number: body.number,
-    id: generateId()
+    id: generateId(),
   }
   persons = persons.concat(newPerson)
   response.json(newPerson)
@@ -65,7 +68,7 @@ app.post('/api/persons', (request, response) => {
 // Delete a contact from the phonebook
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
+  persons = persons.filter((person) => person.id !== id)
   response.status(204).end()
 })
 
@@ -78,6 +81,8 @@ app.get('/info', (request, response) => {
     </div>`
   )
 })
+
+const PORT = 3001
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
